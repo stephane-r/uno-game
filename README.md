5a05a70a2ec6dd8665b8f8a8d8f909c8c89783a7
Stéphane <contact@stephane-richin.fr>
2020-09-08 18:22:10 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Uno Game
+
+TODO
+
+[Game rules](https://www.regles-de-jeux.com/regle-du-uno).
+
+## Stack
+
+- NextJS + KoaJS
+- Socket.IO
+- React
+- TypeScript
---
+++ server/index.ts
@@ -0,0 +1,49 @@
+const Koa = require("koa");
+const next = require("next");
+const Router = require("@koa/router");
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+
+  router.get(
+    "/a",
+    async (ctx: any): Promise<any> => {
+      await app.render(ctx.req, ctx.res, "/a", ctx.query);
+      ctx.respond = false;
+    }
+  );
+
+  router.get(
+    "/b",
+    async (ctx: any): Promise<any> => {
+      await app.render(ctx.req, ctx.res, "/b", ctx.query);
+      ctx.respond = false;
+    }
+  );
+
+  router.all(
+    "*",
+    async (ctx: any): Promise<any> => {
+      await handle(ctx.req, ctx.res);
+      ctx.respond = false;
+    }
+  );
+
+  server.use(
+    async (ctx: any, next: any): Promise<any> => {
+      ctx.res.statusCode = 200;
+      await next();
+    }
+  );
+
+  server.use(router.routes());
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
