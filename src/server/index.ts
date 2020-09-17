42f37899fb0a03bfd6a392816e1073bf6d7fa37c
Stéphane <stephane@novaway.fr>
2020-09-17 09:00:35 +0200

    :heavy_plus_sign: Adding redis server and client

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
-const path = require("path");
-const Koa = require("koa");
-const Router = require("@koa/router");
+const path = require('path');
+const Koa = require('koa');
+const Router = require('@koa/router');
+const redis = require('redis');
+
+const client = redis.createClient();
 
 // Next steps :
 //  - list all users in room on client
@@ -10,10 +13,10 @@ const port = 3001;
 
 const app = new Koa();
 const router = new Router();
-const server = require("http").createServer(app.callback());
-const io = require("socket.io")(server);
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server);
 
-io.on("connection", (socket: any): void => {
+io.on('connection', (socket: any): void => {
   const joinRoom = ({
     room,
     username,
@@ -24,35 +27,35 @@ io.on("connection", (socket: any): void => {
     const test = io.sockets.adapter.rooms[room];
 
     if (test && test.length === 2) {
-      return socket.emit("roomFull");
+      return socket.emit('roomFull');
     }
 
     return socket.join(room, (): void => {
-      io.to(room).emit("joined", { id: socket.id, username });
+      io.to(room).emit('joined', { id: socket.id, username });
     });
   };
 
   const leaveRoom = (): void => {
-    console.log("leave");
+    console.log('leave');
   };
 
   const leavingRoom = (): void => {
     const rooms = Object.keys(socket.rooms);
 
-    rooms.forEach((room) => io.to(room).emit("leaved", socket.id));
+    rooms.forEach((room) => io.to(room).emit('leaved', socket.id));
   };
 
-  socket.on("join", joinRoom);
-  socket.on("disconnecting", leavingRoom);
-  socket.on("disconnect", leaveRoom);
+  socket.on('join', joinRoom);
+  socket.on('disconnecting', leavingRoom);
+  socket.on('disconnect', leaveRoom);
 });
 
 router.get(
-  "/hello-world",
+  '/hello-world',
   async (ctx: any): Promise<any> => {
     ctx.body = {
-      status: "success",
-      json: "test",
+      status: 'success',
+      json: 'test',
     };
   }
 );
